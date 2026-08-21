a8c501ef8e234f0270bde2cd397246c50c48dc83
shiva chetan reddy <shivachethanreddy@gmail.com>
2026-08-21 12:16:07 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum
+{
+  public static void main(String args[])
+  {
+    String s1=args[0];
+    String s2=args[2];
+    int a=Integer.parseInt(s1);
+    int b=Integer.parseInt(s2);
+    System.out.println("Sum="+(a+b));
+  }
+}
+    
