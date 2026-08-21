83a7f189d4647392377f7cc69bab0dd7911f6af1
shiva chetan reddy <shivachethanreddy@gmail.com>
2026-08-21 12:27:59 +0530

    Fix argument index for second input in sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum
   public static void main(String args[])
   {
     String s1=args[0];
-    String s2=args[2];
+    String s2=args[1];
     int a=Integer.parseInt(s1);
     int b=Integer.parseInt(s2);
     System.out.println("Sum="+(a+b));
